1d0a741bd8bb6b903606574cbaccb1e86ce714a4
Gabrielle <gabrielle01.spence@gmail.com>
2022-11-27 18:09:11 -0800

    created list for the book titles and the input

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,40 @@
 
-// Your Code Here
+async function main(){
+    let response = await fetch('http://localhost:3001/listBooks');
+    let bookList = await response.json();
+    bookList.forEach(displayTitle);
+}
+
+/*async function save(book){
+    let adminInput = document.getElementById('adminInput').value;
+    await fetch('http://localhost:3001/updateBook', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'applications/json'
+        },
+        body: JSON.stringify({
+            id: book.id,
+            quantity: adminInput
+        })
+    })
+}*/
+
+function displayTitle(book){
+    let root = document.getElementById('root');
+    
+    root.innerHTML += `
+        <ul>
+            <li>
+                <label>
+                ${book.title}
+                </label>
+                <input id = 'adminInput'>
+                <button onclick = 'save()'> Save </button>
+            </li>
+        </ul>
+    `
+    
+
+}
+
+main();
